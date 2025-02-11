13a684cde0b74fc534e6df0cc11225ff93e05500
iftharkamal <iftharkamal@gmail.com>
2025-02-11 14:53:19 +0530

    motion updates

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,20 +1,29 @@
 import React from "react";
+import {motion} from "framer-motion"
 
 const HeroSection = () => {
   return (
     <>
       {/* navbar only shown in mobile display*/}
-      <nav className="md:hidden flex items-center justify-between mb-5">
+      <motion.nav 
+      initial={{ opacity: 0, y: -30 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 0.6 }}
+      className="md:hidden flex items-center justify-between mb-5">
         <img
           src="/logoWithName.png"
           alt="logo"
           className="w-[122px] h-[42px]"
         />
         <img src="/menu.png" alt="menubar" />
-      </nav>
+      </motion.nav>
 
       {/* container section */}
-      <div className='bg-[url("/mainbackground.png")] bg-cover bg-center h-auto relative rounded-[40px] lg:rounded-[75px] text-[var(--text-color)] font-[var(--font-secondary)] lg:h-[80vh]'>
+      <motion.div
+      initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 2 }}
+       className='bg-[url("/mainbackground.png")] bg-cover bg-center h-auto relative rounded-[40px] lg:rounded-[75px] text-[var(--text-color)] font-[var(--font-secondary)] lg:h-[80vh]'>
         {/* ------inverter border radius with logo in desktop view--------- */}
         <div className="hidden md:block">
           {/* inverted border-radius  */}
@@ -40,12 +49,18 @@ const HeroSection = () => {
         </div>
 
         {/* decorate elements */}
-        <img
+        <motion.img
+           initial={{ opacity: 0, x: -50 }}
+           animate={{ opacity: 1, x: 0 }}
+           transition={{ duration: 1.2, delay: 0.2 }}
           src="/ellipseBlue.png"
           alt="blue"
           className="w-[220px] h-[117px] md:w-[330px] md:h-[190px] lg:w-[420px] lg:h-[270px] absolute top-[97px] left-[-13px] md:left-10 md:top-44  lg:left-10 lg:top-52 xl:left-30 "
         />
-        <img
+        <motion.img
+           initial={{ opacity: 0, x: 50 }}
+           animate={{ opacity: 1, x: 0 }}
+           transition={{ duration: 1.2, delay: 0.2 }}
           src="/ellipseGreen.png"
           alt="green"
           className="w-[220px] h-[117px] md:w-[340px] md:h-[200px] lg:w-[460px] lg:h-[270px] absolute top-[35px] right-[10px] md:right-20 md:top-15 lg:right-10 lg:top-16 xl:right-30"
@@ -53,38 +68,63 @@ const HeroSection = () => {
 
         {/* text/Headlines */}
         <div className="relative z-10">
-          <h5 className="lg:hidden max-w-[220px] font-[var(--font-secondary)] p-6 text-base leading-[26px] opacity-85">
+          <motion.h5 
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1.2, delay: 0.4 }}
+          className="lg:hidden max-w-[220px] font-[var(--font-secondary)] p-6 text-base leading-[26px] opacity-85">
             Looking to streamline your marketing?
-          </h5>
+          </motion.h5>
           <div className="leading-[120px]  lg:leading-[150px] xl:leading-[180px] lg:absolute w-full lg:top-20 xl:top-12 ">
             <div className="overflow-hidden w-full">
-              <h1 className="text-[90px] lg:[120px] font-bold  uppercase scroll-left">
+              <motion.h1 
+               initial={{ x: "-100%" }}
+               animate={{ x: "0%" }}
+               transition={{ duration: 2, ease: "easeOut" }}
+              className="text-[90px] lg:[120px] font-bold  uppercase scroll-left">
                 Don’t just Keep Up, OUTRUN
-              </h1>
+              </motion.h1>
             </div>
             <div className="overflow-hidden ">
-              <h1 className="text-[90px] lg:[120px] font-bold  whitespace-nowrap uppercase scroll-right">
+              <motion.h1 
+              initial={{ x: "100%" }}
+              animate={{ x: "0%" }}
+              transition={{ duration: 2, ease: "easeOut", delay: 0.3 }}
+              className="text-[90px] lg:[120px] font-bold  whitespace-nowrap uppercase scroll-right">
                 Don’t just Keep Up, OUTRUN
-              </h1>
+              </motion.h1>
             </div>
           </div>
-          <h5 className="max-w-[280px] lg:max-w-[330px] font-secondary p-6 text-base lg:text-[18px] leading-[24px] lg:leading-[27px] opacity-85 lg:absolute lg:right-10 lg:top-96 xl:top-96 xl:right-24 ">
+          <motion.h5
+           initial={{ opacity: 0, y: 20 }}
+           animate={{ opacity: 1, y: 0 }}
+           transition={{ duration: 1.2, delay: 0.5 }}
+          className="max-w-[280px] lg:max-w-[330px] font-secondary p-6 text-base lg:text-[18px] leading-[24px] lg:leading-[27px] opacity-85 lg:absolute lg:right-10 lg:top-96 xl:top-96 xl:right-24 ">
             Wisetree Brands is your trusted partner to help you stand out, craft
             sustainable B2B strategies, and create a lasting impression.
-          </h5>
+          </motion.h5>
         </div>
 
         {/* sphere */}
-        <div className="w-[150px] lg:w-[225px] h-[150px] lg:h-[225px] absolute z-20 rounded-full bg-linear-180 from-[#052842] via-[#2A5EA5] to-[#7BB885] top-[150px] right-[50px] md:top-60 md:right-50 lg:top-64 lg:right-60 xl:right-96">
+        <motion.div
+         initial={{opacity:0, x:200}}
+         animate={{opacity:1, x:0}}
+         transition={{duration:2, delay:0.5}}
+         whileHover={{ scale: 1.1 }}
+         className="w-[150px] lg:w-[225px] h-[150px] lg:h-[225px] absolute z-20 rounded-full bg-linear-180 from-[#052842] via-[#2A5EA5] to-[#7BB885] top-[150px] right-[50px] md:top-60 md:right-50 lg:top-64 lg:right-60 xl:right-96">
           <div className="w-full h-full rounded-full bg-transparent relative">
             <p className="text-[35px] lg:text-[50px] text-center font-medium  uppercase leading-10 lg:leading-[60px] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
               Let’s Talk
             </p>
           </div>
-        </div>
+        </motion.div>
 
         {/* button only shown in desktop screen */}
-        <button className="hidden lg:block absolute bottom-3 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-[50px] bg-[#FCFFF9] cursor-pointer  group transition-all duration-500 ">
+        <motion.button
+        initial={{opacity:0, y:-30}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:2, delay:0.5}}
+        className="hidden lg:block absolute bottom-3 left-1/2 transform -translate-x-1/2 px-6 py-3 rounded-[50px] bg-[#FCFFF9] cursor-pointer  group transition-all duration-500 ">
           <div className="flex items-center justify-center">
             <img
               src="/logo.png"
@@ -100,14 +140,14 @@ const HeroSection = () => {
                 />
               </div>
               <div className="w-[134px] h-[51px] rounded-[50px] bg-[#187EB4] flex items-center justify-center">
-                <p className="text-[#FCFFF9E5] font-[var(--font-secondary)] text-[20px] opacity-0 w-0 overflow-hidden transition-all ease-in-out duration-700 group-hover:opacity-100 group-hover:pr-26 group-hover:whitespace-nowrap">
+                <p className="text-[#FCFFF9E5] font-[var(--font-secondary)] text-[20px] opacity-0 w-0 overflow-hidden transition-all ease-in-out duration-700 group-hover:opacity-100 group-hover:pr-28 group-hover:whitespace-nowrap">
                   Contact Us
                 </p>
               </div>
             </div>
           </div>
-        </button>
-      </div>
+        </motion.button>
+      </motion.div>
     </>
   );
 };
